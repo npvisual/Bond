b2e53161b9c4df2a2dea01415c2936f15ef3d961
Nick <npvisual@users.noreply.github.com>
2020-08-18 14:15:08 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
         .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
     ],
     products: [
-        .library(name: "Bond", targets: ["Bond"]),
-        .library(name: "BondStatic", type: .static, targets: ["Bond"]),
-        .library(name: "BondDynamic", type: .dynamic, targets: ["Bond"])
+        .library(name: "Bond", targets: ["Bond"])
     ],
     dependencies: [
         .package(url: "https://github.com/npvisual/ReactiveKit.git", .branch("fix/spm-multilibs")),
@@ -18,10 +16,6 @@ let package = Package(
     targets: [
         .target(name: "BNDProtocolProxyBase"),
         .target(name: "Bond", dependencies: ["BNDProtocolProxyBase", "ReactiveKit", "Differ"]),
-//        .target(name: "BondStatic", dependencies: ["BNDProtocolProxyBase", .product(name: "ReactiveKitStatic", package: "ReactiveKit"), "Differ"]),
-//        .target(name: "BondDynamic", dependencies: ["BNDProtocolProxyBase", .product(name: "ReactiveKitDynamic", package: "ReactiveKit"), "Differ"]),        
-//         .target(name: "BondStatic", dependencies: ["BNDProtocolProxyBase", "ReactiveKitStatic", "Differ"]),
-//         .target(name: "BondDynamic", dependencies: ["BNDProtocolProxyBase", "ReactiveKitDynamic", "Differ"]),
         .testTarget(name: "BondTests", dependencies: ["Bond", "ReactiveKit"])
     ]
 )
