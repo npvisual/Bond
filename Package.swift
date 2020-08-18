b16b61a6dbb7e3c6bacfb04e5672c1215955d186
Nick <npvisual@users.noreply.github.com>
2020-08-18 15:42:05 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "BNDProtocolProxyBase"),
-        .target(name: "BondDynamic", path: "Bond" dependencies: ["BNDProtocolProxyBase", "ReactiveKitDynamic", "Differ"]),
+        .target(name: "BondDynamic", path: "Bond", dependencies: ["BNDProtocolProxyBase", "ReactiveKitDynamic", "Differ"]),
         .target(name: "Bond", dependencies: ["BNDProtocolProxyBase", "ReactiveKit", "Differ"]),
         .testTarget(name: "BondTests", dependencies: ["Bond", "ReactiveKit"])
     ]
