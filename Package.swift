07ccf8b56ece9d9671201f4be95be46e82053550
Nick <npvisual@users.noreply.github.com>
2020-08-18 14:39:37 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
     ],
     products: [
-        .library(name: "Bond", type: .dynamic, targets: ["Bond"])
+        .library(name: "BondDynamic", type: .dynamic, targets: ["Bond"])
     ],
     dependencies: [
         .package(url: "https://github.com/npvisual/ReactiveKit.git", .branch("fix/spm-dynamic")),
